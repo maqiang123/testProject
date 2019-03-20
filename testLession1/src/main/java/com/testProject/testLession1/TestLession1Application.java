1fca025a6ec139f67caec802748e8642009d8cf7
maqiang <ma_qiang@hoperun.com>
2019-03-20 14:38:38 +0800

    dsd

---
+++ testLession1/src/main/java/com/testProject/testLession1/TestLession1Application.java
@@ -0,0 +1,13 @@
+package com.testProject.testLession1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLession1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLession1Application.class, args);
+	}
+
+}
